f4cff38afefa61f6057daa282cf2344a1345f7db
brbbrb <85380887+brbbrb@users.noreply.github.com>
2021-09-21 21:57:24 -0400

    most bugs worked out

--- app.py
+++ app.py
@@ -75,13 +75,24 @@ def stations():
     session = Session(engine)
 
     # Perform a query to retrieve all stations
-    stations = session.query(station.station).all()
+    station_info = session.query(station.station, station.name).all()
 
     # Close session
     session.close()
 
+    # Create station list of dictionaries
+    station_list = []
+    
+    for st in station_info:
+        station_dict = {}
+
+        station_dict["station"] = st[0]
+        station_dict["name"] = st[1]
+
+        station_list.append(station_dict)
+
     # Return JSON list of stations from dataset
-    return jsonify(stations)
+    return jsonify(station_list)
 
 @app.route("/api/v1.0/tobs")
 def tobs():
@@ -123,7 +134,11 @@ def temps_from_start(start):
     # Perform a query to retrieve temperature values greater to or equal to start date
     results = session.query(func.min(measurement.tobs).label('min_temp'), func.max(measurement.tobs).label('max_temp'), func.avg(measurement.tobs).label('avg_temp'))\
         .filter(measurement.date >= start).all()
+    
+    # Close session
     session.close()
+
+# Create a list of temperature stats
     stats_tobs = []
     for r in results:
         tobs_dict = {}
@@ -133,16 +148,23 @@ def temps_from_start(start):
 
         stats_tobs.append(tobs_dict)
 
+    # Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start date
     return jsonify(f"Start date:{start}",stats_tobs)
 
 @app.route("/api/v1.0/<start>/<end>")
 def stats_end(start,end):
-    # Create our session (link) from Python to the DB
+    # Create session from Python to database
     session = Session(engine)
+    
+    # Perform a query to retrieve temperatures between start date and end date
     results = session.query(func.min(measurement.tobs).label('min_temp'), func.max(measurement.tobs).label('max_temp'), func.avg(measurement.tobs).label('avg_temp'))\
         .filter(measurement.date >= start)\
         .filter(measurement.date<= end).all()
+    
+    # Close session
     session.close()
+    
+    # Create a list of temperature stats
     stats_tobs = []
     for r in results:
         tobs_dict = {}
@@ -152,6 +174,7 @@ def stats_end(start,end):
 
         stats_tobs.append(tobs_dict)
 
+    # Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start-end range
     return jsonify(f"Start date:{start}",f"End date:{end}",stats_tobs)
 
 
